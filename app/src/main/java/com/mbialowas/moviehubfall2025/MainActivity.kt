2ab7d6db3b91bd3e0dd5c05f6e6aa6daa882bb3d
Michael Bialowas <mbialowas@rrc.ca>
2025-10-15 14:46:36 -0500

    passing movie id

--- app/src/main/java/com/mbialowas/moviehubfall2025/MainActivity.kt
+++ app/src/main/java/com/mbialowas/moviehubfall2025/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mbialowas.moviehubfall2025
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +26,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mbialowas.moviehubfall2025.Navigation.BottomNav
 import com.mbialowas.moviehubfall2025.api.MovieManager
+import com.mbialowas.moviehubfall2025.api.model.Movie
 import com.mbialowas.moviehubfall2025.destinations.Destination
 import com.mbialowas.moviehubfall2025.screens.Counter
 import com.mbialowas.moviehubfall2025.screens.MovieScreen
@@ -70,6 +76,19 @@ class MainActivity : ComponentActivity() {
                         }
                         composable(Destination.Watch.route) {
                             WatchScreen()
+                        }
+                        composable(Destination.MovieDetail.route){navBackStackEntry->
+                            var movie by remember {
+                                mutableStateOf<Movie?>(null)
+                            }
+                            val movie_id:String?  = navBackStackEntry.arguments?.getString("movieID")
+
+                            Log.i("MJB", movie_id.toString() )
+//                            MovieDetailScreen(
+//                                modifier = Modifier,
+//                                movie = m
+//                            )
+
                         }
                     }
                 }
--- app/src/main/java/com/mbialowas/moviehubfall2025/destinations/Destination.kt
+++ app/src/main/java/com/mbialowas/moviehubfall2025/destinations/Destination.kt
@@ -7,4 +7,5 @@ open class Destination(val route:String) {
     object Movie: Destination("movie")
     object Search : Destination("search")
     object Watch : Destination("watch")
+    object MovieDetail: Destination("movieDetail/{movieID}")
 }
